3bdeea77a29f2d74a09cadce9f7893d380ccc53f
brandylarramore <brandy.larramore@gmail.com>
2023-10-07 14:50:35 -0400

    end of class push. Need to come back to the last few

--- scripts/joins-exercise-script.sql
+++ scripts/joins-exercise-script.sql
@@ -41,17 +41,43 @@ WHERE s.film_title = 'Toy Story 4'
 -- 4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies 
 -- table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
 
-
+SELECT d.company_name, COUNT()
+FROM distributors AS d
+INNER JOIN specs AS s
+ON (s.domestic_distributor_id = d.distributor_id)
+GROUP BY d.company_name, s.film_title
 
 -- 5. Write a query that returns the five distributors with the highest average movie budget.
 
-
-
+SELECT d.company_name name, r.film_budget
+FROM distributors AS d
+INNER JOIN specs AS s
+ON (s.domestic_distributor_id = d.distributor_id)
+INNER JOIN revenue AS r
+USING (movie_id)
+GROUP BY d.company_name, r.film_budget
 
 -- 6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
 
+SELECT d.headquarters, s.film_title, MAX(r.imdb_rating) AS highest_imdb_rating
+FROM distributors AS d
+JOIN specs AS s
+ON (s.domestic_distributor_id = d.distributor_id)
+INNER JOIN rating AS r
+USING (movie_id)
+WHERE d.headquarters NOT LIKE '%CA%'
+GROUP BY d.headquarters, s.film_title
+ORDER BY highest_imdb_rating DESC
+LIMIT 1;
 
+-- 6A. 2, Dirty Dancing
 
 -- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
 
+SELECT
+FROM
 
+
+
+For 7, the questions reads "-- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?"
+You will separate them as over 2 (121 mins and above.) hours, and 2 hours and under (120 and below).
