bb154454e5fb2b654e435255df2b376c9e7ae6e6
brandylarramore <brandy.larramore@gmail.com>
2023-10-10 18:29:12 -0400

    Final finished project

--- scripts/joins-exercise-script.sql
+++ scripts/joins-exercise-script.sql
@@ -8,6 +8,8 @@ ORDER BY r.worldwide_gross
 
 -- 1A. Semi-Tough, 1977, 37,187,139
 
+
+
 -- 2. What year has the highest average imdb rating?
 
 SELECT s.film_title, s.release_year, r.imdb_rating
@@ -17,6 +19,7 @@ USING (movie_id)
 ORDER BY imdb_rating DESC
 
 -- 2A. 2008
+
 
 -- 3. What is the highest grossing G-rated movie? Which company distributed it?
 
@@ -41,21 +44,31 @@ WHERE s.film_title = 'Toy Story 4'
 -- 4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies 
 -- table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
 
-SELECT d.company_name, COUNT()
+SELECT d.company_name, COUNT(distributor_id) AS movies_count
 FROM distributors AS d
-INNER JOIN specs AS s
+LEFT JOIN specs AS s
 ON (s.domestic_distributor_id = d.distributor_id)
-GROUP BY d.company_name, s.film_title
+GROUP BY d.company_name
+ORDER BY movies_count DESC;
+
+-- Double checking I have all distributors (23)
+SELECT DISTINCT company_name
+FROM distributors
+
 
 -- 5. Write a query that returns the five distributors with the highest average movie budget.
 
-SELECT d.company_name name, r.film_budget
+SELECT d.company_name AS name, AVG(r.film_budget) AS avg_budget
 FROM distributors AS d
 INNER JOIN specs AS s
 ON (s.domestic_distributor_id = d.distributor_id)
 INNER JOIN revenue AS r
 USING (movie_id)
-GROUP BY d.company_name, r.film_budget
+GROUP BY d.company_name
+ORDER BY avg_budget DESC
+LIMIT 5;
+
+
 
 -- 6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
 
@@ -74,10 +87,31 @@ LIMIT 1;
 
 -- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
 
+SELECT AVG(imdb_rating) AS avg_rating, length_in_min
+FROM specs AS s
+FULL JOIN rating as r
+USING (movie_id)
+WHERE length_in_min > 120
+GROUP BY length_in_min
+
+
+(SELECT AVG(imdb_rating) AS avg_rating, length_in_min
+FROM specs AS s
+FULL JOIN rating as r
+USING (movie_id)
+WHERE length_in_min <= 120
+GROUP BY length_in_min)
+
+
 SELECT
-FROM
+  CASE
+    WHEN length_in_min > 120 THEN 'Greater than 120 minutes'
+    WHEN length_in_min <= 120 THEN '120 minutes or less'
+  END AS length_category,
+  AVG(imdb_rating) AS avg_rating
+FROM specs AS s
+LEFT JOIN rating AS r USING (movie_id)
+GROUP BY length_category
+ORDER BY length_category;
 
-
-
-For 7, the questions reads "-- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?"
-You will separate them as over 2 (121 mins and above.) hours, and 2 hours and under (120 and below).
+--Greater than 120 minutes
